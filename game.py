7a57c23fa068bc54e63ab6a1e55ea2f0c7ef85ad
sharmaastha15 <155767965+sharmaastha15@users.noreply.github.com>
2024-01-14 20:02:03 +0530

    game.py

--- game.py
+++ game.py
@@ -1,1 +1,32 @@
 
+import random
+options = ("rock","paper","scissor")
+
+playing=True
+while playing:
+    
+    computer = random.choice(options)
+    player=None
+
+    while player not in options:
+      player=input("enter the choice (rock, paper,scissor):")
+
+    print("player:",player)
+    print("computer:",computer)
+
+#winning conditions
+    if player==computer:
+     print("Its tie!")
+    elif player =="rock" and computer== "scissor":
+     print("you win!")
+    elif player=="scissor" and computer =="rock":
+     print("I win!")
+    elif  player=="rock" and computer =="paper":
+     print("I win!")
+    elif  player=="paper" and computer =="scissor":
+     print("I win!")
+    elif  player=="paper" and computer =="rock":
+     print(" you win!")
+    else :
+     print("you win!")
+     
